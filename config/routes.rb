890c491d01c953e44e3f8082a99fab6743bc85a9
yuki kondo <jinteng.yougui@gmail.com>
2023-06-15 11:22:38 +0900

    usersのcontrollerを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'sessions/new'
   resources :sessions, only: [:new, :create, :destroy]
-  resources :users
+  resources :users, only: [:new]
   resources :pictures do
     collection do
       post :confirm
