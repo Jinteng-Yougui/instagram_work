c9ea69caecfa5f15878f1e7bcfd34d972f2b4aeb
yuki kondo <jinteng.yougui@gmail.com>
2023-06-15 12:02:09 +0900

    usersのnew.html.erbにエラー部分を入力

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'sessions/new'
   resources :sessions, only: [:new, :create, :destroy]
-  resources :users, only: [:new]
+  resources :users, only: [:new, :create]
   resources :pictures do
     collection do
       post :confirm
